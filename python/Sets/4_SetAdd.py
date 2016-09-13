5499e49fea6365a4d8630062e9e2e330bd325f0b
root <bryce@brycefury.com>
2016-09-13 18:45:46 +0000

    Update, Reformat, add Captains Room

---
+++ python/Sets/4_SetAdd.py
@@ -0,0 +1,57 @@
+'''
+
+Hackerank Domains: https://www.hackerrank.com/domains
+
+Python https://www.hackerrank.com/domains/python
+
+Sets: https://www.hackerrank.com/domains/python/py-sets
+
+Hackerrank - Python - Sets - Set.add()
+
+If we want to add a single element to an existing set, we can use the .add() operation. 
+It adds the element to the set and returns 'None'.
+
+Example
+
+>>> s = set('HackerRank')
+>>> s.add('H')
+>>> print s
+set(['a', 'c', 'e', 'H', 'k', 'n', 'r', 'R'])
+>>> print s.add('HackerRank')
+None
+>>> print s
+set(['a', 'c', 'e', 'HackerRank', 'H', 'k', 'n', 'r', 'R'])
+
+Task
+
+Apply your knowledge of the .add() operation to help your friend Rupal.
+
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of NN country stamps.
+
+Find the total number of distinct country stamps.
+
+Input Format
+
+The first line contains an integer NN, the total number of country stamps.
+The next NN lines contains the countrey's name where the stamp is from. 
+Constraints
+
+0<N<10000<N<1000
+Output Format
+
+Output the total number of distinct country stamps on a single line.
+
+contact: bryce@brycefury.com
+
+'''
+
+
+noStamps = int(input())
+
+stamps = set()
+
+for i in range(0, noStamps):
+    stamps.add((input()))
+               
+print (len(stamps))
+
---
+++ python/Sets/5_SetDiscardRemoveAndPop.py
@@ -0,0 +1,97 @@
+'''
+
+Hackerank Domains: https://www.hackerrank.com/domains
+
+Python https://www.hackerrank.com/domains/python
+
+Sets: https://www.hackerrank.com/domains/python/py-sets
+
+Hackerrank - Python - Sets - Set .discard(), .remove() & .pop()
+
+.remove(x)
+This operation removes element xx from the set. 
+If element xx does not exist, it raises a KeyError.
+The .remove(x) operation returns None.
+
+Example
+
+>>> s = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+>>> s.remove(5)
+>>> print s
+set([1, 2, 3, 4, 6, 7, 8, 9])
+>>> print s.remove(4)
+None
+>>> print s
+set([1, 2, 3, 6, 7, 8, 9])
+>>> s.remove(0)
+KeyError: 0
+.discard(x)
+This operation also removes element xx from the set. 
+If element xx does not exist, it does not raise a KeyError.
+The .discard(x) operation returns None.
+
+Example
+
+>>> s = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+>>> s.discard(5)
+>>> print s
+set([1, 2, 3, 4, 6, 7, 8, 9])
+>>> print s.discard(4)
+None
+>>> print s
+set([1, 2, 3, 6, 7, 8, 9])
+>>> s.discard(0)
+>>> print s
+set([1, 2, 3, 6, 7, 8, 9])
+.pop()
+This operation removes and return an arbitrary element from the set. 
+If there are no elements to remove, it raises a KeyError.
+
+Example
+
+>>> s = set([1])
+>>> print s.pop()
+1
+>>> print s
+set([])
+>>> print s.pop()
+KeyError: pop from an empty set
+Task
+You have a non-empty set ss, and you have to execute NN commands given in NN lines.
+
+The commands will be pop, remove and discard.
+
+Input Format
+
+The first line contains integer nn, the number of elements in the set ss. 
+The second line contains nn space separated elements of set ss. All of the elements are non-negative integers, less than or equal to 9. 
+The third line contains integer NN, the number of commands.
+The next NN lines contains either pop, remove and/or discard commands followed by their associated value.
+
+Constraints
+
+0<n<200<n<20 
+0<N<200<N<20
+Output Format
+
+Print the sum of the elements of set ss on a single line.
+
+contact: bryce@brycefury.com
+
+'''
+
+
+n = int(input())
+s = set(map(int, input().split()))
+cmdno = int(input())
+
+for i in range(0, cmdno):
+    cmds = input().split()
+    if cmds[0] == 'pop':
+        s.pop()
+    elif cmds[0] == 'remove':
+        s.remove(int(cmds[1]))
+    elif cmds[0] == 'discard':
+        s.discard(int(cmds[1]))
+
+print (sum(s))
