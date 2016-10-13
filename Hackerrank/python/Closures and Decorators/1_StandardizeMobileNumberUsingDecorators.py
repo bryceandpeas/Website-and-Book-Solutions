c29547e7db107eb13fb494c17ec141d2e83360ad
Bryce Fury <brycefury@ymail.com>
2016-10-13 14:29:57 +0100

    Fix filenames, remove old

---
+++ Hackerrank/python/Closures and Decorators/1_StandardizeMobileNumberUsingDecorators.py
@@ -0,0 +1,44 @@
+"""
+
+Monday 25th April 2016
+
+Hackerrank - Closures and Decorators - Standardize Mobile Number Using Decorators
+
+Let's dive into decorators! You are given NN mobile numbers. Sort them in ascending order then print them in the standard format shown below:
+
+
++91 xxxxx xxxxx
+
+The given mobile numbers may have +91+91, 9191 or 00 written before the actual 1010 digit number. Alternatively, there may not be any prefix at all. 
+
+Input Format
+
+The first line of input contains an integer NN, the number of mobile phone numbers. 
+NN lines follow each containing a mobile number.
+
+Output Format
+
+Print NN mobile numbers on separate lines in the required format.
+
+
+"""
+
+n = int(input())
+numbers = [input() for i in range(n)]
+
+def sort_number_list(function):
+    def list_wrap(inputlist):
+        print (*sorted(function(inputlist)), sep = '\n')
+    return (list_wrap)
+
+
+@sort_number_list
+def number_formatter(pn):
+    return (['+91 ' + i[-10:-5] + ' ' + i[-5:] for i in pn])
+
+number_formatter(numbers)
+
+        
+
+
+
---
+++ Hackerrank/python/Math/7_TriangleQuest.py
@@ -0,0 +1,36 @@
+"""
+
+February 2016
+
+Hackerrank - Python - Math - Triangle Quest
+
+You are given a positive integer NN. Print a numerical triangle of height N−1N−1 like the one below:
+
+1
+22
+333
+4444
+55555
+......
+Can you do it using only arithmetic operations, a single for loop and print statement?
+
+Use no more than two lines. The first line (the for statement) is already written for you. You have to complete the print statement.
+
+Note: Using anything related to strings will give a score of 00.
+
+Input Format 
+A single line containing integer, NN.
+
+Constraints 
+1≤N≤91≤N≤9
+
+Output Format 
+Print N−1N−1 lines as explained above.
+
+
+"""
+
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print (i*(((10**i)-1)//9))
+
