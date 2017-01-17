bf48d8c7f7e29586dcd67a39a1409a1c46b11268
Bryce Fury <brycefury@ymail.com>
2017-01-17 01:01:28 +0000

    Add StringRepeat Crystal solution

---
+++ Codewars Katas/8Kyu/Crystal/StringRepeat.cr
@@ -0,0 +1,12 @@
+# Codewars Kata - 8 Kyu - String repeat
+#
+# Description:
+#
+# Write a function called repeatStr which repeats the given string string exactly n times.
+#
+# repeatStr(6, "I") // "IIIIII"
+# repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+def repeat_str (n, s)
+  s * n
+end
