68d42bb8b58101ab9b99a1dc61a571e660493374
Bryce Fury <brycefury@ymail.com>
2017-01-17 00:38:33 +0000

    Move Python solutions to /Python

---
+++ Codewars Katas/8Kyu/Python/Grasshopper_CombineStrings.py
@@ -0,0 +1,19 @@
+'''
+
+Codewars Kata - 8 Kyu - Grasshopper - Combine strings
+
+Description:
+
+Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combine_names('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+'''
+
+def combine_names(first, last):
+    return(first + ' ' + last)
