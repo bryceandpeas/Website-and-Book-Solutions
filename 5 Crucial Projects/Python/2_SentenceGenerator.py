1baec4c4a5571a37f4dec0411672df181d765118
Bryce Fury <brycefury@ymail.com>
2017-01-18 10:18:35 +0000

    Fix 5 Crucial Projects Python paths

--- 5 Crucial Projects/Python/2_SentenceGenerator.py
+++ 5 Crucial Projects/Python/2_SentenceGenerator.py
@@ -1,13 +1,12 @@
 '''
-
 5 Crucial Projects for Beginners - https://www.daniweb.com/programming/software-development/threads/131973/5-crucial-projects-for-beginners
 
 2 - Sentence Generator:
 
 Overview:
+
 A series of different parts of sentences will be randomly 
 put together to come up with new interesting sentences.
-
 What you will be Using:
 Random, Integers, Print, Strings, Breaks, Functions, For, Range
 My Thoughts on Project:
@@ -15,7 +14,6 @@ A very fun beginning project, have fun with this...
 Come up with some wacky sentences! 
 Also, this project really gets you to experiment with strings, 
 piecing them together, randoming from a set of strings, and more.
-
 '''
 
 import random
@@ -44,8 +42,6 @@ sentence_list = [''.join(random.choice(word_list)) for i in range(random.randint
 capitalized_list = [i.title() for i in sentence_list]
 
 for i in capitalized_list:
-	final_text += '{0}.\n'.format(i)
+  final_text += '{0}.\n'.format(i)
 
 print(final_text)
-
-
