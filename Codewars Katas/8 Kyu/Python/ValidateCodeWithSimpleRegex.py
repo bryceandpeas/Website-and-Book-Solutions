e842623215573e9cd0607d037d87ffd2f4a70f6b
Bryce Fury <brycefury@ymail.com>
2017-01-20 04:46:23 +0000

    Remove trailing whitespace

--- Codewars Katas/8 Kyu/Python/ValidateCodeWithSimpleRegex.py
+++ Codewars Katas/8 Kyu/Python/ValidateCodeWithSimpleRegex.py
@@ -4,7 +4,7 @@ Codewars Kata - 8 Kyu - validate code with simple regex
 
 Description:
 
-Basic regex tasks. Write a function that takes in a numeric code of any lenght. 
+Basic regex tasks. Write a function that takes in a numeric code of any lenght.
 The function will check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
 
 You can assume the input will always be a nuber.
