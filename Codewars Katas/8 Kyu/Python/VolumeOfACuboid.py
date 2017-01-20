276feb88afb29acf74746b856122fc535d9e56c8
Bryce Fury <brycefury@ymail.com>
2017-01-20 04:46:45 +0000

    Remove trailing whitespace

--- Codewars Katas/8 Kyu/Python/VolumeOfACuboid.py
+++ Codewars Katas/8 Kyu/Python/VolumeOfACuboid.py
@@ -4,7 +4,7 @@ Codewars Kata - 8 Kyu - Volume of a Cuboid
 
 Description:
 
-Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. 
+Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid.
 Write a function to help Bob with this calculation.
 
 Ruby: def get_volume_of_cuboid(length, width, height)
