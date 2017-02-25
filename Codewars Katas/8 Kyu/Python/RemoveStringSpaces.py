00db1bc90be89429404519cffd58d9916ccf03dd
Bryce Fury <brycefury@ymail.com>
2017-02-25 23:39:29 +0000

    Add Remove String Spaces python solution

---
+++ Codewars Katas/8 Kyu/Python/RemoveStringSpaces.py
@@ -0,0 +1,13 @@
+'''
+
+Codewars Kata - 8 Kyu - Remove String Spaces
+
+Description:
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+'''
+
+def no_space(x):
+    return (x.replace(' ', ''))
+    
