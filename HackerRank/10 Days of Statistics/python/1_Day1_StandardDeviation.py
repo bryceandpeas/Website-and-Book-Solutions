ffa9416113586d029ccf31dbf715fa9892e24927
BryceFury <brycefury@ymail.com>
2017-07-24 18:44:48 +0000

    Add Linux Shell Bash Looping and Skipping solution

--- HackerRank/10 Days of Statistics/python/1_Day1_StandardDeviation.py
+++ HackerRank/10 Days of Statistics/python/1_Day1_StandardDeviation.py
@@ -18,7 +18,27 @@ Your answer should be in decimal form, rounded to a scale of  decimal place
 '''
 
 n = int(input().strip())
-x = [int(i) for i in input().strip().split()]
+x = [int(i) for i in input().strip().sn = int(input())
+nums = sorted([int(num) for num in input().split()])
+
+def median(nums):
+    if (len(nums) % 2) == 0:
+        return int(sum(nums[len(nums) // 2-1:len(nums) // 2+1]) / 2)
+    else:
+        return nums[len(nums) // 2]
+
+def quartiles(n, nums):
+    q1 = median(nums[:len(nums) // 2])
+    q2 = median(nums)
+    if (n % 2)  == 0:
+        q3 = median(nums[len(nums) // 2:])
+    else:
+        q3 = median(nums[len(nums) // 2+1:])
+    return (q1, q2, q3)
+
+q1, q2, q3 = quartiles(n, nums)
+print('{0}{3}{1}{3}{2}'.format(q1, q2, q3, '\n'))
+plit()]
 
 m = (sum(x) / n)
 v = (sum(((i - m) ** 2) for i in x) / n)
