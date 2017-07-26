e4e4cc46375e0f45892e1b8c8ae97b6e6810dd1e
BryceFury <brycefury@ymail.com>
2017-07-26 19:27:08 +0000

    Add Linux Shell Bash A Personalized Echo solution

---
+++ HackerRank/Linux Shell/Bash/APersonalizedEcho.sh
@@ -0,0 +1,12 @@
+#
+# Hackerank Domains: https://www.hackerrank.com/domains
+#
+# Bash https://www.hackerrank.com/domains/shell/bash
+#
+# Hackerrank - Linux Shell - Bash - A Personalized Echo
+#
+# Write a Bash script which accepts  as input and displays a greeting: "Welcome (name)"
+#
+
+read -p "Enter your name: " name; echo "Welcome $name"
+
