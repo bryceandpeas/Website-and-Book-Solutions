a78842887ad9a23553239d42515d0aa11f4ef77a
Bryce Fury <brycefury@ymail.com>
2018-06-25 22:53:54 +0100

    Add 1_GuessMyNumber.cr solution

--- Cue.py
+++ Cue.py
@@ -16,15 +16,19 @@ from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
 
 root = 'C:\\Users\\bryce\\Desktop\\Website-Solutions'
+compare_root = 'C:\\Users\\bryce\\Documents\\Website-Solutions'
 
 print(os.getcwd())
 
 file_list = []
+compare_file_list = []
 date_list = []
 commit_list = []
 today = datetime.today()
-date = datetime.today() - timedelta(days=260)
+date = datetime.today() - timedelta(days=259)
 weekday = datetime.weekday(date)
+
+
 
 while weekday < 6:
 	date = date + timedelta(1)
@@ -39,33 +43,46 @@ for path, subdirs, files in os.walk(root):
 		else:
 			file_list.append(os.path.join(path, name))
 
+for path, subdirs, files in os.walk(compare_root):
+	subdirs[:] = [d for d in subdirs if d not in exclude]
+	for name in files:
+		if name.endswith(".gitignore") or name.endswith(".md"):
+			continue
+		else:
+			compare_file_list.append(os.path.join(path, name))
+
 for file in file_list:
-	date_list.append(date)
-	rand_hour = random.randint(18, 23)
-	rand_minute = random.randint(00, 59)
-	rand_second = random.randint(00, 59)
-	date += timedelta(days=1)
-	date = date.replace(hour=rand_hour, minute=rand_minute, second=rand_second)
+	if file not in compare_file_list:
+		date_list.append(date)
+		rand_hour = random.randint(18, 23)
+		rand_minute = random.randint(00, 59)
+		rand_second = random.randint(00, 59)
+		date += timedelta(days=1)
+		date = date.replace(hour=rand_hour, minute=rand_minute, second=rand_second)
 
 count = 0
 
 for file in file_list:
-	filename = str(file).rsplit('\\', 1)[1]
-	commit_message = 'Add {0} solution'.format(filename)
-	os.environ["GIT_COMMITTER_DATE"] = "{0}".format(date_list[count])
-	print('Set commiter date to: ')
-	#print(subprocess.Popen(["set", "GIT_COMMITTER_DATE",], stdout=subprocess.PIPE))
-	os.environ["GIT_AUTHOR_DATE"] = "{0}".format(date_list[count])
-	print('Set author date to: ')
-	#print(subprocess.Popen(["set", "GIT_AUTHOR_DATE"], stdout=subprocess.PIPE))
-	subprocess.Popen(["git", "add", "{0}".format(file)], stdout=subprocess.PIPE)
-	print('Added: {0}'.format(file))
-	subprocess.Popen(["git", "commit", "--allow-empty", "-m" "{0}".format(commit_message)], stdout=subprocess.PIPE)
-	print('Commit message: "' + commit_message+ '"')
-	subprocess.Popen(["git", "push", "origin", "master"], stdout=subprocess.PIPE)
-	print('Pushed')
-	count += 1
-	print('Sleep 20')
-	time.sleep(20)
+	if file not in compare_file_list:
+		filename = str(file).rsplit('\\', 1)[1]
+		commit_message = 'Add {0} solution'.format(filename)
+		os.environ["GIT_COMMITTER_DATE"] = "{0}".format(date_list[count])
+		print('Set commiter date to: ')
+		#print(subprocess.Popen(["set", "GIT_COMMITTER_DATE",], stdout=subprocess.PIPE))
+		os.environ["GIT_AUTHOR_DATE"] = "{0}".format(date_list[count])
+		print('Set author date to: ')
+		#print(subprocess.Popen(["set", "GIT_AUTHOR_DATE"], stdout=subprocess.PIPE))
+		subprocess.Popen(["git", "add", "{0}".format(file)], stdout=subprocess.PIPE)
+		print('Added: {0}'.format(file))
+		subprocess.Popen(["git", "commit", "--allow-empty", "-m" "{0}".format(commit_message)], stdout=subprocess.PIPE)
+		print('Commit message: "' + commit_message+ '"')
+		subprocess.Popen(["git", "push", "origin", "master"], stdout=subprocess.PIPE)
+		print('Pushed')
+		count += 1
+		print('Sleep 20')
+		time.sleep(20)
+	else:
+		print('Skipping: ' + str(file) + '[Already Exists]')
+		time.sleep(20)
 
 print('Processed {0} files'.format(count))
