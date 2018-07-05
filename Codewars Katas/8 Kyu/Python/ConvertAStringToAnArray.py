b941ecebb92b99cca90f6344119777f23a1ac2a6
Bryce Fury <brycefury@ymail.com>
2018-07-05 18:59:59 +0100

    Add ConvertAStringToAnArray.py solution

---
+++ Codewars Katas/8 Kyu/Python/ConvertAStringToAnArray.py
@@ -0,0 +1,16 @@
+'''
+
+Codewars Kata - 8 Kyu - Convert a string to an array
+
+Description:
+
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+'''
+
+def string_to_array(s):
+    return(s.split(' '))
