a4cb030cac5bf916fe5136d2d6397fa9449e18ff
Bryce Fury <brycefury@ymail.com>
2018-07-11 19:52:55 +0100

    Add YouOnlyNeedOne_Beginner.py solution

---
+++ Codewars Katas/8 Kyu/Python/YouOnlyNeedOne_Beginner.py
@@ -0,0 +1,16 @@
+'''
+
+Codewars Kata - 8 Kyu - You only need one - Beginner
+
+Description:
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+'''
+
+def check(seq, elem):
+    return True if elem in seq else False
