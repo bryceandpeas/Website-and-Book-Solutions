d16969e5e9a98af3a631474f8875a0de514a175e
Bryce Fury <brycefury@ymail.com>
2018-07-23 19:12:37 +0100

    Add BumpsInTheRoad.py solution

---
+++ Codewars Katas/7 Kyu/Python/BumpsInTheRoad.py
@@ -0,0 +1,13 @@
+'''
+
+Codewars Kata - 7 Kyu - Bumps in the Road
+
+Description:
+
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy! Given a string showing either flat road ("_") or bumps ("n"), work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+'''
+
+def bumps(road):
+    return("Woohoo!" if road.count("n") <= 15 else "Car Dead")
