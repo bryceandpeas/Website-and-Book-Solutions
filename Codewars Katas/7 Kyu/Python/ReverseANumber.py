82ec3f8e628c8812427f9a5bfa9d0455569dbcdd
Bryce Fury <brycefury@ymail.com>
2018-08-14 20:37:25 +0100

    Add ReverseANumber.py solution

---
+++ Codewars Katas/7 Kyu/Python/ReverseANumber.py
@@ -0,0 +1,19 @@
+'''
+
+Codewars Kata - 7 Kyu - Reverse a Number
+
+Description:
+
+Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+Examples
+ 123 ->  321
+-456 -> -654
+1000 ->    1
+
+'''
+
+def reverse_number(n):
+    return(int(str(n)[::-1]) if n > 0 else (int((str(n)[::-1]).replace('-','')) * (-1)))
