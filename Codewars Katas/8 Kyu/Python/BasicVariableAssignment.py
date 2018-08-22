b6d9f891ce6414461d50313a7ccc595f38c1d8cb
Bryce Fury <brycefury@ymail.com>
2018-08-22 22:52:41 +0100

    Add BasicVariableAssignment.py solution

---
+++ Codewars Katas/8 Kyu/Python/BasicVariableAssignment.py
@@ -0,0 +1,13 @@
+'''
+
+Codewars Kata - 8 Kyu - Basic variable assignment
+
+Description:
+
+This code should store "codewa.rs" as a variable called "name" but it's not working. Can you figure out why?
+
+'''
+
+a = "code"
+b = "wa.rs"
+name = a + b
