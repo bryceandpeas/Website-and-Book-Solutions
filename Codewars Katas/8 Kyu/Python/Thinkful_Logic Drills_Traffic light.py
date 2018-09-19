693f37fd01b16d5795ea66b9b05ded70e792c54b
Bryce Fury <brycefury@ymail.com>
2018-09-19 21:01:05 +0100

    Add Thinkful_Logic Drills_Traffic light.py solution

---
+++ Codewars Katas/8 Kyu/Python/Thinkful_Logic Drills_Traffic light.py
@@ -0,0 +1,16 @@
+'''
+
+Codewars Kata - 8 Kyu - Thinkful - Logic Drills: Traffic light
+
+Description:
+
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, update_light('green') should return 'yellow'.
+
+'''
+
+def update_light(current):
+    return('green' if current == 'red' else ('yellow' if current == 'green' else 'red'))
