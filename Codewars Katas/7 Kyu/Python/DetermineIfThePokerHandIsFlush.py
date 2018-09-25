54307636cc82f56c2d3e268d1b81701420e308f8
Bryce Fury <brycefury@ymail.com>
2018-09-25 20:16:17 +0100

    Add DetermineIfThePokerHandIsFlush.py solution

---
+++ Codewars Katas/7 Kyu/Python/DetermineIfThePokerHandIsFlush.py
@@ -0,0 +1,23 @@
+'''
+
+Codewars Kata - 7 Kyu - Determine if the poker hand is flush
+
+Description:
+
+Determine if the poker hand is flush, meaning if the five cards are of the same suit.
+
+Your function will be passed a list/array of 5 strings, each representing a poker card in the format "5H" (5 of hearts), meaning the value of the card followed by the initial of its suit (Hearts, Spades, Diamonds or Clubs). No jokers included.
+
+Your function should return true if the hand is a flush, false otherwise.
+
+The possible card values are 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+
+Examples
+["AS", "3S", "9S", "KS", "4S"]  ==> true
+
+["AD", "4S", "7H", "KS", "10S"] ==> false
+
+'''
+
+def is_flush(cards):
+    return(True if len(set(''.join([i[-1] for i in cards]))) == 1 else False)
