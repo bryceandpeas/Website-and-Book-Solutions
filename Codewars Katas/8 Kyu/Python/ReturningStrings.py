e93d5c865b20e8e2314b6b6a072d18b7bf5f6214
Bryce Fury <brycefury@ymail.com>
2018-10-07 18:53:35 +0100

    Add ReturningStrings.py solution

---
+++ Codewars Katas/8 Kyu/Python/ReturningStrings.py
@@ -0,0 +1,14 @@
+'''
+
+Codewars Kata - 8 Kyu - Returning Strings
+
+Description:
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+*[Make sure you type the exact thing I wrote or the program may not execute properly]*
+
+'''
+
+def greet(name):
+    return("Hello, {0} how are you doing today?".format(name))
