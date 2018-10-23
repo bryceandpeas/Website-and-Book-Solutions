0c85ea43708716eaebc31e88368a8df636435c8f
Bryce Fury <brycefury@ymail.com>
2018-10-23 23:58:41 +0100

    Add ForUFCFans.py solution

---
+++ Codewars Katas/8 Kyu/Python/ForUFCFans.py
@@ -0,0 +1,22 @@
+'''
+
+Codewars Kata - 8 Kyu - For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+
+Description:
+
+This is a beginner friendly kata especially for UFC/MMA fans.
+
+It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
+
+If the winner is George Saint Pierre he will obviously say:
+
+"I am not impressed by your performance."
+If the winner is Conor McGregor he will most undoubtedly say:
+
+"I'd like to take this chance to apologize.. To absolutely NOBODY!"
+Good Luck!
+
+'''
+
+def quote(fighter):
+    return("I am not impressed by your performance." if fighter.lower() == 'george saint pierre' else "I'd like to take this chance to apologize.. To absolutely NOBODY!")
