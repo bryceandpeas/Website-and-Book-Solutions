d86d04010c96819aee09b2cf1a5a8b828ce0d2b9
Bryce Fury <brycefury@ymail.com>
2018-10-25 22:00:00 +0100

    Add Grasshopper_Debug.py solution

---
+++ Codewars Katas/8 Kyu/Python/Grasshopper_Debug.py
@@ -0,0 +1,28 @@
+'''
+
+Codewars Kata - 8 Kyu - Grasshopper - Debug
+
+Description:
+
+Debug celsius converter
+Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius. Unfortunately his code has some bugs.
+
+Find the errors in the code to get the celsius converter working properly.
+
+To convert fahrenheit to celsius:
+
+celsius = (fahrenheit - 32) * (5/9)
+Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
+
+'''
+
+def weather_info(temp):
+    c = convert_to_celsius(temp)
+    if (c > 0):
+        return (str(c) + " is above freezing temperature")
+    else:
+        return (str(c) + " is freezing temperature")
+    
+def convert_to_celsius(temp):
+    celsius = (temp - 32) * 5 / float(9)
+    return celsius
