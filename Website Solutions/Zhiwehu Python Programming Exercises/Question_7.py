79cc2e437d45ed57fbd0653062a370dcb3f87576
BryceFury <brycefury@ymail.com>
2019-03-26 21:27:58 +0000

    Add Question_7.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_7.py
@@ -0,0 +1,28 @@
+'''
+
+Question 7
+Level 2
+
+Question:
+Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+Note: i=0,1.., X-1; j=0,1,¡­Y-1.
+Example
+Suppose the following inputs are given to the program:
+3,5
+Then, the output of the program should be:
+[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]] 
+
+Hints:
+Note: In case of input data being supplied to the question, it should be assumed to be a console input in a comma-separated form.
+
+'''
+
+def gen_array(x, y):
+	generated_array =[]
+	generated_array.append([0 for i in range(0, y)])
+	generated_array.append([i for i in range(0, y)])
+	generated_array.append([i * 2 for i in range(0, y)])
+	return generated_array
+
+x, y = map(int, (input("Please enter your your values in the format \"x, y\": " ).split(',')))
+print(gen_array(x, y))
