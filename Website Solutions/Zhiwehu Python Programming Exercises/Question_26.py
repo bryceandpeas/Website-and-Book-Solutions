02d35c680986afb5714905a13fd6a27e86849110
BryceFury <brycefury@ymail.com>
2019-03-26 23:58:09 +0000

    Add Question_26.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_26.py
@@ -0,0 +1,13 @@
+'''
+
+Question: 26
+
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
+
+'''
+
+def calc_nums(x, y):
+	return(x + y)
