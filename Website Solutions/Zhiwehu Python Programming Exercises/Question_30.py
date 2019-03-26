f83a1a710419f827c349051451cfdd8a7a39c9be
BryceFury <brycefury@ymail.com>
2019-03-26 18:05:11 +0000

    Add Question_30.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_30.py
@@ -0,0 +1,20 @@
+'''
+
+Question: 30
+
+2.10
+
+
+Question:
+Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+Hints:
+
+Use + to concatenate the strings
+
+'''
+
+def concat_str(x, y):
+	return(x + y)
+
+print(concat_str(x, y))
