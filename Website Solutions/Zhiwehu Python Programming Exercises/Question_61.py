12573e0d661d524457bc5ec9e5080d97438db947
BryceFury <brycefury@ymail.com>
2019-03-28 18:19:38 +0000

    Add Question_61.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_61.py
@@ -0,0 +1,16 @@
+'''
+
+Question: 61
+
+Question:
+
+
+Print a unicode string "hello world".
+
+Hints:
+
+Use u'strings' format to define unicode string.
+
+'''
+
+print(u'hello, world' + ' ' +  u'\u2713')
