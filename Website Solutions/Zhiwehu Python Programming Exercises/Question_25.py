d3c2d33d4cdca28d65b6ec6dc1d39f88587ec77d
BryceFury <brycefury@ymail.com>
2019-03-29 23:45:18 +0000

    Add Question_25.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_25.py
@@ -0,0 +1,20 @@
+'''
+
+Question 25
+Level 1
+
+Question:
+    Define a class, which have a class parameter and have a same instance parameter.
+
+Hints:
+    Define a instance parameter, need add it in __init__ method
+    You can init a object with construct parameter or set the value later
+
+'''
+
+class HorriblyConfusingQuestion(object):
+	"""docstring for HorriblyConfusingQuestion"""
+	def __init__(self, arg):
+		super(HorriblyConfusingQuestion, self).__init__()
+		self.confusion = confusion
+		
