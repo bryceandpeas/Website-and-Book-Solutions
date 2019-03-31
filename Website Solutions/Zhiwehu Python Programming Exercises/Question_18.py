9d48496bd28f5e54f283bae918480b84be691f2f
BryceFury <brycefury@ymail.com>
2019-03-31 23:51:46 +0100

    Add Question_18.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_18.py
@@ -0,0 +1,78 @@
+'''
+
+Question 18
+Level 3
+
+Question:
+A website requires the users to input username and password to register. Write a program to check the validity of password input by users.
+Following are the criteria for checking the password:
+1. At least 1 letter between [a-z]
+2. At least 1 number between [0-9]
+1. At least 1 letter between [A-Z]
+3. At least 1 character from [$#@]
+4. Minimum length of transaction password: 6
+5. Maximum length of transaction password: 12
+Your program should accept a sequence of comma separated passwords and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma.
+Example
+If the following passwords are given as input to the program:
+ABd1234@1,a F1#,2w3E*,2We3345
+Then, the output of the program should be:
+ABd1234@1
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+
+'''
+
+import getpass
+
+def password_check(passwords):
+	acceptable_passwords = []
+	lower_letter = False
+	upper_letter = False 
+	number = False
+	special = False 
+	length = False
+
+	for password in passwords:
+
+		if len(password) < 6:
+			length = False
+		elif len(password) > 12:
+			length = False
+		else:
+			length = True
+
+		for j in password:
+			if j.islower():
+				lower_letter = True
+				break
+			else:
+				lower_letter = False
+
+		for k in password:
+			if k.isupper():
+				upper_letter = True
+				break
+			else:
+				upper_letter = False
+
+		number = any(l.isdigit() for l in password)
+
+		for m in password:
+			if m in ['$', '#', '@']:
+				special = True
+				break
+			else:
+				special = False
+
+		if lower_letter and upper_letter and number and special and length:
+			acceptable_passwords.append(password)
+		else:
+			continue
+
+	return(acceptable_passwords)
+
+passwords = input("Please enter the passwords you wish to check as a comma seperated sting: ").split(',')
+
+print(password_check(passwords))
