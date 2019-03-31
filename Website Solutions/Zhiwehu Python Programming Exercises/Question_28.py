f35c10d8a2dc710a2cce21cd039ac5a7a62b887b
BryceFury <brycefury@ymail.com>
2019-03-31 19:02:20 +0100

    Add Question_28.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_28.py
@@ -0,0 +1,23 @@
+'''
+
+Question: 28
+
+Define a function that can convert a integer into a string and print it in console.
+
+Hints:
+
+Use str() to convert a number to string.
+
+'''
+
+def int_str(number):
+	return(str(number))
+
+print(int_str(number))
+
+
+'''
+
+This question is a repeat of question 27
+
+'''
