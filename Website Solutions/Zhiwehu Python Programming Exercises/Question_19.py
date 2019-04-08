7ecd54020e6bf307398db09d01b1358ef3892c44
BryceFury <brycefury@ymail.com>
2019-04-08 22:57:45 +0100

    Add Question_19.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_19.py
@@ -0,0 +1,47 @@
+'''
+
+Question 19
+Level 3
+
+Question:
+You are required to write a program to sort the (name, age, height) tuples by ascending order where name is string, age and height are numbers. The tuples are input by console. The sort criteria is:
+1: Sort based on name;
+2: Then sort based on age;
+3: Then sort by score.
+The priority is that name > age > score.
+If the following tuples are given as input to the program:
+Tom,19,80
+John,20,90
+Jony,17,91
+Jony,17,93
+Json,21,85
+Then, the output of the program should be:
+[('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+We use itemgetter to enable multiple sort keys.
+
+'''
+
+def get_people():
+	print("Please enter your people in the format \"name, age, height\" " + "\n")
+
+	person_amount = int(input("How many people would you like to enter?: "))
+
+	print("\n")
+
+	people = []
+	for i in range(person_amount):
+	    people.append(tuple((input("Please enter person {0}: ".format(i + 1))).replace(' ', '').split(',')))
+
+	return people
+
+def sort_people(people):
+	output_list = sorted(people, key=lambda element: (element[0], element[1], element[2]))
+
+	return output_list
+
+people = get_people()
+
+print(sort_people(people))
