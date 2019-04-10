7b08d5de34a5a9dd2bd56e7fcb03568bd74a6f75
BryceFury <brycefury@ymail.com>
2019-04-10 22:29:47 +0100

    Add Question_50.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_50.py
@@ -0,0 +1,17 @@
+'''
+
+Question: 50
+
+7.2
+
+Question:
+Define a class named American which has a static method called printNationality.
+
+Hints:
+
+Use @staticmethod decorator to define class static method.
+
+'''
+
+def gen_list(input_number):
+	return([i**2 for i in range(input_number + 1)])[5:]
