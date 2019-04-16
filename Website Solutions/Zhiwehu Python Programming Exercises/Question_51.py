fd7fb040f1d66b8cb31dc8214a95e1ef19ebfd1d
BryceFury <brycefury@ymail.com>
2019-04-16 19:05:52 +0100

    Add Question_51.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_51.py
@@ -0,0 +1,29 @@
+'''
+
+Question: 51
+
+7.2
+
+Question:
+Define a class named American and its subclass NewYorker. 
+
+Hints:
+
+Use class Subclass(ParentClass) to define a subclass.
+
+'''
+
+class American(object):
+	"""docstring for American"""
+	def __init__(self):
+		super(American, self).__init__()
+		
+		
+
+class NewYorker(American):
+	"""docstring for NewYorker"""
+	def __init__(self):
+		super(NewYorker, self).__init__()
+		
+		
+		
