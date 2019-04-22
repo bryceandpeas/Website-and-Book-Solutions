452fcfc1dc310781f02543b6194ab2aa7bbb8b03
BryceFury <brycefury@ymail.com>
2019-04-22 23:23:01 +0100

    Add Question_37.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_37.py
@@ -0,0 +1,19 @@
+'''
+
+Question: 37
+
+2.10
+
+Question:
+Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included).
+
+Hints:
+
+Use ** operator to get power of a number.
+Use range() for loops.
+Use list.append() to add values into a list.
+
+'''
+
+def gen_list(input_number):
+	return[i for i in range(input_number + 1)]
