eecf477028410bdad4a68231828324334d797030
BryceFury <brycefury@ymail.com>
2019-04-29 23:53:51 +0100

    Add Question_40.py solution

---
+++ Website Solutions/Zhiwehu Python Programming Exercises/Question_40.py
@@ -0,0 +1,20 @@
+'''
+
+Question: 40
+
+2.10
+
+Question:
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print all values except the first 5 elements in the list.
+
+Hints:
+
+Use ** operator to get power of a number.
+Use range() for loops.
+Use list.append() to add values into a list.
+Use [n1:n2] to slice a list
+
+'''
+
+def gen_list(input_number):
+	return([i**2 for i in range(input_number + 1)])[5:]
